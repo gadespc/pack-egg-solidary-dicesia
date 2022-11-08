a3de24f1742663b4723c489be095d03c52737b1b
gadespc <tsmjony@gmail.com>
2022-11-08 01:51:40 +0100

    Upgrade ThirdWEB SDK V3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-// import ThirdwebGuideFooter from "../components/ThirdwebGuideFooter";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Polygon;
@@ -10,7 +9,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <Component {...pageProps} />
-      {/* <ThirdwebGuideFooter /> */}
     </ThirdwebProvider>
   );
 }
